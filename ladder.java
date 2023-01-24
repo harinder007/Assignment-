561c745ae0148c27716860dabfb4fc88345d35f9
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-24 22:56:07 +0530

    if else ladder

---
+++ ladder.java
@@ -0,0 +1,14 @@
+public class ladder {  
+    public static void main(String[] args) {  
+    String city = "Meerut";  
+    if(city == "Meerut") {  
+    System.out.println("city is meerut");  
+    }else if (city == "Noida") {  
+    System.out.println("city is noida");  
+    }else if(city == "Agra") {  
+    System.out.println("city is agra");  
+    }else {  
+    System.out.println(city);  
+    }  
+    }  
+    }  
