62d9351a2d7adb950c48c2f36081568aed07191e
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-24 22:31:24 +0530

    nested if else statement

---
+++ nested.java
@@ -0,0 +1,19 @@
+public class nested {
+    public static void main(String args[]){
+        int a=10;
+
+        if(a==11 || a<20){
+            if(a<11){
+                System.out.println("a is smaller than 10");
+             }
+             if(a<20){
+                System.out.println("a is smaller than 20 also");
+             }
+        }
+        else{
+            System.out.println("a is greater than 20");
+        }
+    
+    }
+    
+}
