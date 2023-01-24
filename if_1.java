f57009fe0dc7d7a4f7128358416150a785b68c0d
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-24 22:30:05 +0530

    simple if-else statement

---
+++ if_1.java
@@ -0,0 +1,12 @@
+public class if_1 {
+    public static void main(String args[]){
+        int age=18;
+        if(age==18){
+            System.out.println("You are Adult");
+        }
+        else{
+            System.out.println("You are Teenager");
+        }
+    }
+    
+}
