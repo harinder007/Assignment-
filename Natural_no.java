0d3b1b5fccd9a779358d1a88c1a5e69af7086187
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-01 22:23:07 +0530

    first 10 natural no.

---
+++ Natural_no.java
@@ -0,0 +1,17 @@
+package ControlStatement;
+// print  first 10 natural no.
+
+public class Natural_no {
+	
+
+	public static void main(String[] args) {
+		System.out.println("First natural no. is");
+		for(int i=1;i<=10;i++)
+		{
+			
+			System.out.println(i);
+		}
+		
+	}
+
+}
