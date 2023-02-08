f2be9f97b120e279ebaa210a9dd5c889174d0c03
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-08 19:21:29 +0530

    result either true or false

---
+++ Double.java
@@ -0,0 +1,15 @@
+package Assignment;
+import java.util.*;
+public class Double {
+public static void main(String[] args) {
+	System.out.println("Enter first number : ");
+	Scanner sc =new Scanner(System.in);
+	double num1=sc.nextDouble();
+	System.out.println("Enter first number : ");
+	double num2=sc.nextDouble();
+	
+	System.out.println(num1>0 && num1>0 && num2>0 && num2>1);
+	
+	//Output is either true or false
+}
+}
