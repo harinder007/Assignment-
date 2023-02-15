af2d7b97274ceb3c03c6f8120f90d3df4dd74027
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-15 22:51:49 +0530

    Activity 2

---
+++ Introduction to java activity-2/My_program.java
@@ -0,0 +1,9 @@
+class DisplayMessage{
+    void printmessage(){
+    System.out.println("hello my message");
+    }}
+    class My_Program extends DisplayMessage{
+    public static void main(String args[]){
+     My_Program program= new  My_Program();
+    program.printmessage();
+    }}
